62c080b397123ee93d8c91eadabf8a9be38def3f
hianshul07 <hianshul07@gmail.com>
2024-07-10 15:28:39 +0530

    working updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
  */
 interface IState {
 	data: ServerRespond[];
-	showGraph: Boolean;
+	showGraph: boolean;
 }
 
 /**
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -34,7 +34,7 @@ class Graph extends Component<IProps, {}> {
 		// Get element to attach the table from the DOM.
 		// const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 		const elem = (document.getElementsByTagName(
-			'`perspective-viewer'
+			'perspective-viewer'
 		)[0] as unknown) as PerspectiveViewerElement;
 
 		const schema = {
@@ -51,20 +51,15 @@ class Graph extends Component<IProps, {}> {
 			// Load the `table` in the `<perspective-viewer>` DOM reference.
 
 			// Add more Perspective configurations here.
-			elem.setAttribute('view', 'y_line');
-			elem.setAttribute('column-pivots', '["stock"]');
-			elem.setAttribute('row-pivots', '["timestamp"]');
-			elem.setAttribute('columns', '["top_ask_price"]');
+			elem.load(this.table);
+			elem.setAttribute('view','y_line');
+			elem.setAttribute('column-pivots','["stock"]');
+			elem.setAttribute('row-pivots','["timestamp"]');
+			elem.setAttribute('columns','["top_ask_price"]');
 			elem.setAttribute(
 				'aggregates',
-				`{
-          "stock": "distinct count",
-          "top_ask_price":"avg",
-          "top_bid_price":"avg"
-          "timestamp":"distinct count",
-        }`
+				'{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}'
 			);
-			elem.load(this.table);
 		}
 	}
 
